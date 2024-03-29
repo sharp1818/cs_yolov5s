9e6122a119ad66e6869a7d1f8029e93a3bc03915
Sharp Santillán <adrian.shura18@gmail.com>
2024-03-29 16:49:27 -0500

    add requirements

---
+++ install.py
@@ -0,0 +1,34 @@
+import subprocess
+import os
+
+def install_git():
+    # Verificar si Git está instalado
+    try:
+        subprocess.run(['git', '--version'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+        print("Git already installed.")
+    except subprocess.CalledProcessError:
+        # Si Git no está instalado, instalarlo
+        print("Installing Git...")
+        subprocess.run(['winget', 'install', '--id', 'Git.Git', '-e', '--source', 'winget'], check=True)
+
+def install_dependencies():
+    # Cambiar al directorio yolov5
+    os.chdir('yolov5')
+    # Instalar las dependencias desde el archivo requirements.txt
+    print("Installing dependencies...")
+    subprocess.run(['pip', 'install', '-r', 'requirements.txt'], check=True)
+
+def clone_repository():
+    # Verificar si el repositorio ya está clonado
+    if os.path.exists('yolov5'):
+        print("YOLOv5 repository already cloned.")
+    else:
+        # Clonar el repositorio de YOLOv5
+        print("Cloning YOLOv5 repository...")
+        subprocess.run(['git', 'clone', 'https://github.com/ultralytics/yolov5.git'], check=True)
+        os.chdir('yolov5')  # Cambiar al directorio clonado
+
+if __name__ == "__main__":
+    install_git()
+    clone_repository()
+    install_dependencies()
